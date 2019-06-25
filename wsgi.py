77c564429a921eed4439bfc8f2137b399c8b172e
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2019-06-24 23:07:27 -0500

    Adding upload dir to wsgi.py file

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,14 @@
 import os
 from server import connex_app as application
-from server import get_args
+from server import flask_app, get_args
 
 activate_this = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              'venv', 'bin', 'activate_this.py')
 
 if __name__ == "__main__":
     args = get_args()
+    flask_app.config['UPLOAD_FOLDER'] = args.file_dir
+
     with open(activate_this) as file_:
         exec(file_.read(), dict(__file__=activate_this))
 
