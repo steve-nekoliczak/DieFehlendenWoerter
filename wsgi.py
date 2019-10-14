4c5844c1ac79ec104f3ffab303ced4e563f7cb70
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2019-10-13 22:29:28 -0500

    making app nginx friendly

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,11 @@
-import os
-
 import config
 from routes import pages, resources
 from args import get_args
+
+app = config.connex_app
 
 
 if __name__ == "__main__":
     args = get_args()
     config.flask_app.config['UPLOAD_FOLDER'] = args.file_dir
-    config.connex_app.run(debug=args.debug, port=args.port)
-
+    app.run(debug=args.debug, port=args.port)
